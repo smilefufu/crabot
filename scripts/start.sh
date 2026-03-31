433a0aa5e033a0df48b52e76a9e866904c84a688
FuFu <fufu.bluesand@gmail.com>
2026-03-31 16:21:39 +0800

    bugfix

--- scripts/start.sh
+++ scripts/start.sh
@@ -38,9 +38,9 @@ build_frontend || exit 1
 
 # 4. Module Manager（前台 exec）
 log_section "启动 Crabot"
-local _offset="${CRABOT_PORT_OFFSET:-0}"
-local _mm_port=$((19000 + _offset))
-local _web_port=$((3000 + _offset))
+_offset="${CRABOT_PORT_OFFSET:-0}"
+_mm_port=$((19000 + _offset))
+_web_port=$((3000 + _offset))
 log_info "Module Manager 启动中 (port $_mm_port) ..."
 log_info "Admin Web: http://localhost:$_web_port"
 cd "$CRABOT_HOME/crabot-core"
